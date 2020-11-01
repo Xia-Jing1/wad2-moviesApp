c2a4a3f9c511610ea706d3b0b52eebb9fc6b2000
Jing Xia <1251816747@qq.com>
2020-11-01 19:08:37 +0000

    Full review page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import FavoriteMoviesPage from './pages/favoritesMoviesPage'
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
           </li>
         </ul>
         <Switch>
+        <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
