420cfca814ddf4b9b0d848a8978c1a32ca7da05e
Jing Xia <1251816747@qq.com>
2020-11-02 20:49:02 +0000

    E2E tests - Home page - Filtering by genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -65,8 +65,19 @@ describe("Home Page ", () => {
       })
 
     })
-    describe("By movie genre" ,() => {
-      // More later
-    })
-  })
-})
+    describe("By movie genre", () => {
+        it("should display movies with the specified genre only", () => {
+          const selectedGenreId = 35;
+          const selectedGenreText = "Comedy";
+          const matchingMovies = filterByGenre(movies, selectedGenreId);
+          cy.get("select").select(selectedGenreText); 
+          cy.get(".card").should("have.length", matchingMovies.length);
+          cy.get(".card").each(($card, index) => {
+            cy.wrap($card)
+              .find(".card-title")
+              .should("have.text", matchingMovies[index].title);
+          });      
+        });
+    });
+  
+  });});
