d70c12f9b4d9717e23576722f4ecfb00b4cb6038
Jing Xia <1251816747@qq.com>
2020-11-11 21:20:25 +0000

    Fix upcoming movies page

--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from "react";
-import StubAPI from "../api/stubAPI";
+import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-import { getUpcomingMovies } from "../api/tmdb-api";
+import {MoviesContext} from '../contexts/moviesContext'
+import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
 const UpcomingPage = () => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
-
-  const addToFavorites = movieId => {
-    setMovies(movies => {
-      const index = movies.map(m => m.id).indexOf(movieId);
-      StubAPI.add(movies[index]);
-      let newMoviesState = [...movies]
-      newMoviesState.splice(index, 1);
-      return newMoviesState;
-    });
-  };
+  const context = useContext(MoviesContext);
+  const movies = context.movies.filter((m) => {  // New
+    return !("favorite" in m);
+  });
 
   return (
-      <PageTemplate
-        title='Upcoming Movies'
-        movies={movies}
-        buttonHandler={addToFavorites}
-      />
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}  /* Changed */
+      action={(movie) => {
+        return <AddToFavoritesButton movie={movie} />;
+      }}
+    />
   );
 };
-
 export default UpcomingPage;
